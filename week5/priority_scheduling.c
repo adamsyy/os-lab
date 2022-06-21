09959c3e59c48c308f960bde944a461eb532842c
sdflkajdfklj <dkfjaklfj@gmail.com>
2022-06-21 14:08:32 +0530

    week5

---
+++ week5/priority_scheduling.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct
+{
+ int pno;
+ int pri;
+ int btime;
+ 
+ int wtime;
+}sp;
+
+int main()
+{
+ int i,j,n;
+ int tbm=0,totwtime=0,totttime=0;
+ sp *p,t;
+ printf("\nPRIORIRY SCHEDULING\n");
+ printf("\n enter the no of procss..\n");
+ scanf("%d",&n);
+ p=(sp*)malloc(sizeof(sp));
+ printf("enter the burst time and poriority:\n");
+ for(i=0;i<n;i++)
+ {
+   printf("process%d:",i+1);
+   scanf("%d%d",&p[i].btime,&p[i].pri);
+   p[i].pno=i+1;
+   p[i].wtime=0;
+   
+ }
+ for(i=0;i<n-1;i++)
+ for(j=i+1;j<n;j++)
+ {
+ if(p[i].pri>p[j].pri)
+ {
+ t=p[i];
+ p[i]=p[j];
+ p[j]=t;
+ }
+ }
+ printf("\n process\tbursttime\twaiting time \tturnaround time\n");
+ for(i=0;i<n;i++)
+ {
+   totwtime+=p[i].wtime=tbm;
+   tbm+=p[i].btime;
+   printf("\n%d\t\t%d",p[i].pno,p[i].btime);
+   printf("\t\t%d\t\t%d",p[i].wtime,p[i].wtime+p[i].btime);
+   }
+ totttime=tbm+totwtime;
+ printf("\n total wating time :%d",totwtime);
+ printf("\naverage wating time:%f",(float)totwtime/n);
+ printf("\n total turnaround time:%d",totttime);
+ printf("\n avg turnaround time:%f",(float)totttime/n);
+}
